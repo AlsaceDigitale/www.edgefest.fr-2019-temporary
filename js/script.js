4f782272fb61f403d650f145bab42acbd48c5c05
Florian Fries <fries.florian@gmail.com>
2015-07-03 00:23:20 +0200

    Prepare player for panimix audio stream

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+	var d = new Date();
+	if(d.getDate() == 4 || location.hash == "#/stream") {
+		$("#streampanimix").css("display", "block");
+		$("#streampanimix audio").attr("autoplay", "autoplay");
+	}
+});
