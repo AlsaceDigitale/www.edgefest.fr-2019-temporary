17df2c1d7f556ae87a209790b5debaab46ec123b
florian <fries.florian@gmail.com>
2016-06-27 19:54:29 +0200

    add youtube videos everywhere

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ $(function() {
 		$("#streampanimix").css("display", "block");
 		$("#streampanimix audio").attr("autoplay", "autoplay");
 	}*/
+
+	$(window).resize(function() {
+		setAspectRatio();
+	});
+
+	setAspectRatio();
 });
+
+function setAspectRatio() {
+  $('iframe').each(function() {
+    $(this).css('height', $(this).width() * 9/16);
+  });
+}
